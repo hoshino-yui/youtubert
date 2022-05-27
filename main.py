2a1fa09facb02ea282f914128ae77fe1542b13f3
Hoshino Yui <105177693+hoshino-yui@users.noreply.github.com>
2022-05-27 22:52:14 +0100

    commit.

--- main.py
+++ main.py
@@ -102,6 +102,7 @@ def extract_channel_or_video(url):
         "ignoreerrors": True,
         "extractor_args": {'youtube': {
             # 'max_comments': ['100'],
+            'skip': ['dash', 'hls'],
             'comment_sort': ['top']}}
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
