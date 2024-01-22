084ed3c0174f8f138472f8acd9e8091f08ac56c9
Hoshino Yui <105177693+hoshino-yui@users.noreply.github.com>
2024-01-22 01:18:39 +0000

    json

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-import json
 import sys
+import logging
+import multiprocessing
+from typing import List
 
 import yt_dlp
 import utils
@@ -9,20 +11,7 @@ from model.comment import Comment
 from model.video import Video
 
 
-def write_video(video: Video):
-    try:
-        file_lines = video.generate_markdown_lines()
-        if file_lines:
-            filename = video.generate_filename()
-            write_file(filename, file_lines)
-    except IOError as e:
-        print(f"Failed to write video {video.video_id} - {video.title}")
-        print(e)
-
-
-def write_file(filename, lines):
-    with utils.create_and_write_file(filename) as file:
-        file.write("\n\n".join(lines))
+WORKERS = 16
 
 
 def extract_timestamp(video):
@@ -59,38 +48,89 @@ def extract_video(video) -> Video:
     )
 
 
-def process_video(video):
+def process_video(video) -> List[Video]:
     video = extract_video(video)
-    write_video(video)
+    if video:
+        return [video]
+    else:
+        return []
 
 
-def extract_entry(entry):
+def extract_entry(entry) -> List[Video]:
     if entry.get('_type') == 'playlist':
+        videos = []
         for sub_entry in entry["entries"]:
             if sub_entry:
-                extract_entry(sub_entry)
+                videos.extend(extract_entry(sub_entry))
+        return videos
     else:
-        process_video(entry)
+        return process_video(entry)
 
 
-def extract_url(url):
+def extract_url(url) -> List[Video]:
     ydl_opts = {
         "getcomments": True,
         "extractor_retries": 3,
         "ignoreerrors": True,
         "extractor_args": {'youtube': {
-            # 'max_comments': ['100'],
             'skip': ['dash'],
             'comment_sort': ['top']}}
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=False)
         info = ydl.sanitize_info(info)
-        # json.dump(info, sys.stdout)
-        # print()
-        extract_entry(info)
+        if info:
+            return extract_entry(info)
+        else:
+            return []
+
+
+def process_urls(info):
+    if info["_type"] == 'playlist':
+        return [video for playlist in info['entries'] for video in process_urls(playlist)]
+    elif info["_type"] == 'url':
+        return [info['url']]
+
+
+def extract_urls(url) -> List[Video]:
+    ydl_opts = {
+        "extractor_retries": 3,
+        "ignoreerrors": True,
+        'extract_flat': True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        info = ydl.sanitize_info(info)
+        urls = process_urls(info)
+        with multiprocessing.Pool(processes=WORKERS) as pool:
+            videos: List[List[Video]] = pool.map(extract_url, urls)
+            return [v for vs in videos for v in vs]
+
+
+def write_channels(videos: List[Video]):
+    videos = [video for video in videos if video.comments]
+    videos = sorted(videos, key=lambda v: v.timestamp)
+    channel_ids = set([video.channel_id for video in videos])
+    for channel_id in channel_ids:
+        channel_videos = [video for video in videos if video.channel_id == channel_id]
+        write_file(channel_videos)
+
+
+def write_file(videos: List[Video]):
+    channel_id = videos[0].channel_id
+    channel_name = videos[0].channel
+    filename = 'data/' + channel_name + ' - ' + channel_id + '.json'
+    with utils.create_and_write_file(filename) as file:
+        file.write(utils.videos_to_json_string(videos))
+
+
+def main(urls):
+    videos: List[Video] = [v for url in urls for v in extract_urls(url)]
+    print("Processed videos: ", len(videos))
+    write_channels(videos)
 
 
 if __name__ == '__main__':
-    for arg in sys.argv[1:]:
-        extract_url(arg)
+    logging.getLogger().setLevel(logging.DEBUG)
+    args = sys.argv[1:]
+    main(args)
