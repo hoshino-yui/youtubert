9278cf186e26732908bcf57c78f52976e38ede85
Hoshino Yui <105177693+hoshino-yui@users.noreply.github.com>
2025-11-02 02:10:35 +0000

    commit

--- youtubert-web/src/App.js
+++ youtubert-web/src/App.js
@@ -1,42 +1,51 @@
 import './App.css';
-import suzuka from './data/鈴花ステラ Ch.  Suzuka Stella - UChAOCCFuF2hto05Z68xp56A'
-import luna from './data/望月ルーナ  Mochitsugi Luna Ch. - UCKiWJEjJw-zGW_Co_kJt5dw'
-import cross from './data/Cross Ch. 月島クロス【HKVTuber】 - UC5bhpsL8ZUHQ9q-HOLVc82A'
-import kurona from './data/Kurona Ch.酒吞玖蘿娜【innoneer.TV】 - UCYPr6jNySMcfpjheFi61U8w.json'
-import tama from './data/QuonTama Ch. 久遠たま - UCBC7vYFNQoGPupe5NxPG4Bw'
-import rumii from './data/Rumii Ch. 如月ルミィ【HKVTuber】 - UCJcKh9mwJH4zhHsU4NDs54g'
-import aisu from './data/小林冰 Aisu Ch.  - UCnwgM2M3C4JOVdOZ0OLu_bA.json'
-import saki from './data/瑠凜紗紀-RuriSaki【HKVtuber】 - UCwwvI-bV0CQ4FwkF-Kg0NBQ'
 
 import React, {useState} from "react";
-import {SearchField, Label, Input, Button} from 'react-aria-components';
+import {Button, Input, Label, SearchField} from 'react-aria-components';
 import VideoSelector from "./VideoSelector";
 import VideoView from "./VideoView";
 import ChannelSelector from "./ChannelSelector";
+import {useQueries} from "@tanstack/react-query";
 
 
-function getVideos() {
-  const videos = new Map();
-  [suzuka, luna, cross, kurona, tama, rumii, aisu, saki]
-    .flatMap(c => c)
-    .forEach(v => videos.set(v.video_id, v));
-  return videos;
+function getAllChannels() {
+  return new Map(Object.entries({
+    'UChAOCCFuF2hto05Z68xp56A': '鈴花ステラ Ch.  Suzuka Stella',
+    'UCKiWJEjJw-zGW_Co_kJt5dw': '望月ルーナ  Mochitsugi Luna Ch.',
+    'UC5bhpsL8ZUHQ9q-HOLVc82A': 'Cross Ch. 月島クロス【HKVTuber】',
+    'UCYPr6jNySMcfpjheFi61U8w': 'Kurona Ch.酒吞玖蘿娜【innoneer.TV】',
+    'UCBC7vYFNQoGPupe5NxPG4Bw': 'QuonTama Ch. 久遠たま',
+    'UCJcKh9mwJH4zhHsU4NDs54g': 'Rumii Ch. 如月ルミィ【HKVTuber】',
+    'UCnwgM2M3C4JOVdOZ0OLu_bA': '小林冰 Aisu Ch.',
+    'UCwwvI-bV0CQ4FwkF-Kg0NBQ': '瑠凜紗紀-RuriSaki【HKVtuber】',
+    'UCNqQfIrZMLNNJNLB0AOnfAg': '真黑makuro【HKVTuber】',
+  }));
 }
 
-function getChannels(videos) {
-  const channels = new Map();
-  videos.forEach(v => channels.set(v.channel_id, v.channel));
-  return channels;
-}
+const retrieveChannelVideos = async ({queryKey: [_, channel_id]}) => {
+  console.log('channel_id', channel_id);
+  const response = await fetch(`youtubert/data/${channel_id}.json`);
+  return await response.json();
+};
 
 function App() {
-  const videosMap = getVideos();
-  const channels = getChannels(Array.from(videosMap.values()));
+  const channels = getAllChannels();
   const [searchText, setSearchText] = useState();
   const [selectedChannel, setSelectedChannel] = useState(new Set([Array.from(channels.keys())[0]]));
   const [selectedVideos, setSelectedVideos] = useState(new Set());
   const theSearchText = searchText?.trim();
 
+  const results = useQueries({
+    queries: Array.from(selectedChannel).map((channel_id) => ({
+      queryKey: ['channel_videos', channel_id],
+      queryFn: retrieveChannelVideos,
+      refetchOnWindowFocus: false,
+      enabled: true
+    }))
+  });
+
+  const videos = results.filter(result => result.isSuccess).flatMap(result => result.data);
+  const videosMap = new Map(videos.map(v => [v.video_id, v]));
 
   let videosList = Array.from(videosMap.values());
   videosList = videosList.filter(v => Array.from(selectedChannel).includes(v.channel_id));
